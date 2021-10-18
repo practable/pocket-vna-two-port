ccd4b7fee015e400b094bb2e2bcdcdc190a9c06d
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-10-18 23:00:26 +0100

    Add cmd/vna

---
+++ cmd/vna/cmd/root.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2021 Tim Drysdale <timothy.d.drysdale@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "vna",
+	Short: "A tool for connecting a pocketVNA to a websocket server",
+	Long: `A tool for connecting a pocketVNA to a websocket server, under the assumption that only one pocketVNA is available (so that getting the first available handle is sufficient). Usage:
+
+To unlock all pocket VNAs on the system (recommended before attempting to stream)
+vna unlock
+
+To stream to a websocket relay (e.g. a github.com/timdrysdale/relay sessionhost)
+vna stream ws://localhost:8888/ws/vna
+.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+}
---
+++ cmd/vna/cmd/unlock.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2021 Tim Drysdale <timothy.d.drysdale@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/timdrysdale/go-pocketvna/pkg/pocket"
+)
+
+// unlockCmd represents the unlock command
+var unlockCmd = &cobra.Command{
+	Use:   "unlock",
+	Short: "Unlock all VNA on the system",
+	Long:  `Unlock all VNA on the system.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := pocket.ForceUnlockDevices()
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		os.Exit(0)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(unlockCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// unlockCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// unlockCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
