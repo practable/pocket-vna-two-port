498c458a01de2d6480fabdafdd932d4f0ea9e9e2
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-01-26 12:05:54 +0000

    Add TestPipeInterfaceToWs to calibration_test

--- pkg/calibration/calibration_test.go
+++ pkg/calibration/calibration_test.go
@@ -1,8 +1,16 @@
 package calibration
 
 import (
+	"context"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/timdrysdale/go-pocketvna/pkg/reconws"
 )
 
 func init() {
@@ -31,7 +39,7 @@ var upgrader = websocket.Upgrader{}
 //
 //	go switchMock(fromClient, toClient, ctx_mock)
 //
-//	// Convert http://127.0.0.1 to ws://127.0.0.
+//	// Convert http://127.0.0.1 to ws://127.0.0.1
 //	u := "ws" + strings.TrimPrefix(s.URL, "http")
 //
 //	c := New(u, ctx)
@@ -182,35 +190,77 @@ var upgrader = websocket.Upgrader{}
 //
 //}
 //
-//func TestPipeInterfaceToWs(t *testing.T) {
-//	timeout := 100 * time.Millisecond
-//
-//	chanWs := make(chan reconws.WsMessage)
-//	chanInterface := make(chan interface{})
-//	ctx, cancel := context.WithCancel(context.Background())
-//	defer cancel()
-//	go PipeInterfaceToWs(chanInterface, chanWs, ctx)
-//
-//	/* Test Request */
-//
-//	chanInterface <- Command{
-//		Set: "port",
-//		To:  "short",
-//	}
-//
-//	select {
-//
-//	case <-time.After(timeout):
-//		t.Error("timeout awaiting response")
-//	case reply := <-chanWs:
-//
-//		expected := "{\"set\":\"port\",\"to\":\"short\"}"
-//
-//		assert.Equal(t, expected, string(reply.Data))
-//	}
-//
-//}
-//
+
+func makeCommand() (Command, error) {
+
+	freq := []uint64{1000, 2000, 3000}
+	short_real := []float64{0.31, 0.32, -0.33}
+	short_imag := []float64{0.41, -0.42, 0.45}
+	open_real := []float64{1.31, 1.32, -1.33}
+	open_imag := []float64{1.41, -1.42, 1.45}
+	load_real := []float64{2.31, 2.32, -2.33}
+	load_imag := []float64{2.41, -2.42, 2.45}
+	dut_real := []float64{3.31, 3.32, -3.33}
+	dut_imag := []float64{3.41, -3.42, 3.45}
+
+	pshort, err := makeSParam(freq, short_real, short_imag)
+	if err != nil {
+		return Command{}, err
+	}
+
+	popen, err := makeSParam(freq, open_real, open_imag)
+	if err != nil {
+		return Command{}, err
+	}
+
+	pload, err := makeSParam(freq, load_real, load_imag)
+	if err != nil {
+		return Command{}, err
+	}
+
+	pdut, err := makeSParam(freq, dut_real, dut_imag)
+	if err != nil {
+		return Command{}, err
+	}
+
+	return MakeOnePort(pshort, popen, pload, pdut)
+
+}
+
+func TestPipeInterfaceToWs(t *testing.T) {
+	timeout := 100 * time.Millisecond
+
+	chanWs := make(chan reconws.WsMessage)
+	chanInterface := make(chan interface{})
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go PipeInterfaceToWs(chanInterface, chanWs, ctx)
+
+	/* Test Request */
+
+	c, err := makeCommand()
+
+	assert.NoError(t, err)
+
+	chanInterface <- c
+
+	select {
+
+	case <-time.After(timeout):
+		t.Error("timeout awaiting response")
+	case reply := <-chanWs:
+
+		var cc Command
+
+		err := json.Unmarshal([]byte(reply.Data), &cc)
+
+		assert.NoError(t, err)
+
+		assert.True(t, cmp.Equal(c, cc))
+	}
+
+}
+
 //func TestPipeWsToInterface(t *testing.T) {
 //	timeout := 100 * time.Millisecond
 //
@@ -237,12 +287,13 @@ var upgrader = websocket.Upgrader{}
 //	case <-time.After(timeout):
 //		t.Error("timeout awaiting response")
 //	case reply := <-chanInterface:
-//		assert.Equal(t, reflect.TypeOf(reply), reflect.TypeOf(Report{}))
-//		report := reply.(Report)
-//		assert.Equal(t, "short", report.Is)
+//		assert.Equal(t, reflect.TypeOf(reply), reflect.TypeOf(Result{}))
+//		report := reply.(Result)
+//		assert.Equal(t, "short", Result.Is)
 //	}
 //
 //}
+
 //
 //// Reply with expected response if port command is correctly formed
 //
