b95e58a530f3971be05aa4883afb465a199c77a8
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-01-30 23:41:53 +0000

    Fix rfswitch test

--- pkg/rfswitch/request_test.go
+++ pkg/rfswitch/request_test.go
@@ -140,33 +140,30 @@ func TestNew(t *testing.T) {
 		t.Error("timeout waiting for reply to Set port")
 	}
 
-	// check that the commands complete ok when getting the expected response
-	// repeat many times to check for intermittent erros
-	for i := 0; i < 1000; i++ {
+	// note this is mocked so repeating
 
-		err := rf.SetShort()
+	err := rf.SetShort()
 
-		assert.NoError(t, err)
+	assert.NoError(t, err)
 
-		err = rf.SetOpen()
+	err = rf.SetOpen()
 
-		assert.NoError(t, err)
+	assert.NoError(t, err)
 
-		err = rf.SetLoad()
+	err = rf.SetLoad()
 
-		assert.NoError(t, err)
+	assert.NoError(t, err)
 
-		err = rf.SetDUT()
+	err = rf.SetDUT()
 
-		assert.NoError(t, err)
-	}
+	assert.NoError(t, err)
 
 	cancel_mock() //check commands are asking for the right port
 
 	go rf.SetShort()
 
 	msg := <-fromClient
-	err := json.Unmarshal([]byte(msg.Data), &c)
+	err = json.Unmarshal([]byte(msg.Data), &c)
 	assert.NoError(t, err)
 	assert.Equal(t, "port", c.Set)
 	assert.Equal(t, "short", c.To)
