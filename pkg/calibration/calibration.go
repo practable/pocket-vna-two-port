913357d4b2e0ed474b2c2f6d04fb6155789b7679
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-11-17 20:33:58 +0000

    calibration(fix): tests for twoport

--- pkg/calibration/calibration.go
+++ pkg/calibration/calibration.go
@@ -13,6 +13,282 @@ import (
 	"github.com/timdrysdale/go-pocketvna/pkg/reconws"
 )
 
+type Calibration struct {
+	u        string
+	R        *reconws.ReconWs
+	Ctx      context.Context
+	Request  chan interface{}
+	Response chan interface{}
+	Timeout  time.Duration
+	Command  Command
+	Scan     interface{}
+}
+
+/* Command object definition in python calibration service
+
+   {
+    "cmd":"twoportport",
+    "freq": np.linspace(1e6,100e6,num=N),
+    "short":{
+        "s11": {
+            "real":np.random.rand(N),
+            "imag":np.random.rand(N),
+            },
+         "s12" : {
+            "real":np.random.rand(N),
+            "imag":np.random.rand(N),
+            },
+        "s21": {
+            "real":np.random.rand(N),
+            "imag":np.random.rand(N),
+            },
+         "s22" : {
+            "real":np.random.rand(N),
+            "imag":np.random.rand(N),
+            },
+      },
+     "open":{ //as for short  },
+     "load":{ // as for short },
+     "load":{ // as for short },
+     "dut":{ // as for short  }
+    }
+*/
+
+// Command represents a twoport calibration command (request)
+type Command struct {
+	Command string   `json:"cmd"`
+	Freq    []uint64 `json:"freq"`
+	Short   Result   `json:"short"`
+	Open    Result   `json:"open"`
+	Load    Result   `json:"load"`
+	Thru    Result   `json:"thru"`
+	DUT     Result   `json:"dut"`
+}
+
+/* 2-port Result object definition in python calibration service
+{
+   "freq": network.f,
+   "s11": {
+      "real": np.squeeze(network.s_re[:,0,0]),
+      "imag": np.squeeze(network.s_im[:,0,0]),
+           },
+  "s12" : {
+      "real": np.squeeze(network.s_re[:,0,1]),
+      "imag": np.squeeze(network.s_im[:,0,1]),
+     },
+ "s21": {
+      "real": np.squeeze(network.s_re[:,1,0]),
+      "imag": np.squeeze(network.s_im[:,1,0]),
+     },
+  "s22" : {
+      "real": np.squeeze(network.s_re[:,1,1]),
+      "imag": np.squeeze(network.s_im[:,1,1]),
+     },
+   }
+*/
+
+type Result struct {
+	Freq []uint64     `json:"freq"`
+	S11  ComplexArray `json:"s11"`
+	S12  ComplexArray `json:"s12"`
+	S21  ComplexArray `json:"s21"`
+	S22  ComplexArray `json:"s22"`
+}
+
+type SParam struct {
+	S11 ComplexArray `json:"s11"`
+	S12 ComplexArray `json:"s12"`
+	S21 ComplexArray `json:"s21"`
+	S22 ComplexArray `json:"s22"`
+}
+
+type ComplexArray struct {
+	Real []float64 `json:"real"`
+	Imag []float64 `json:"imag"`
+}
+
+// MakeTwoPort assembles measurements into a twoport calibration command
+
+func MakeTwoPort(pshort1, pshort2, popen1, popen2, pload1, pload2, pthru, pdut []pocket.SParam) (Command, error) {
+
+	ls := []int{
+		len(pshort1),
+		len(pshort2),
+		len(popen1),
+		len(popen2),
+		len(pload1),
+		len(pload2),
+		len(pthru),
+		len(pdut),
+	}
+
+	for _, l := range ls {
+		if l != ls[0] {
+			return Command{}, errors.New("inputs are not the same length")
+		}
+	}
+
+	short1 := PocketToResult(pshort1)
+	short2 := PocketToResult(pshort2)
+	open1 := PocketToResult(popen1)
+	open2 := PocketToResult(popen2)
+	load1 := PocketToResult(pload1)
+	load2 := PocketToResult(pload2)
+	thru := PocketToResult(pthru)
+	dut := PocketToResult(pdut)
+
+	short := CombineTwoReflectiveStandards(short1, short2)
+	open := CombineTwoReflectiveStandards(open1, open2)
+	load := CombineTwoReflectiveStandards(load1, load2)
+
+	return Command{
+		Command: "twoport",
+		Freq:    short.Freq,
+		Short:   short,
+		Open:    open,
+		Load:    load,
+		Thru:    thru,
+		DUT:     dut,
+	}, nil
+
+}
+
+// CombineTwoReflectiveStandards combines S11 from the first argument,
+// with S22 from the second, and sets S12, S21 to zero
+func CombineTwoReflectiveStandards(r1, r2 Result) Result {
+	z := make([]float64, len(r1.S11.Real))
+
+	caz := ComplexArray{
+		Real: z,
+		Imag: z,
+	}
+
+	return Result{
+		Freq: r1.Freq,
+		S11:  r1.S11,
+		S12:  caz,
+		S21:  caz,
+		S22:  r2.S22,
+	}
+}
+
+// CombineTwoReflectiveStandards combines S11 from the first argument,
+// with S22 from the second, and sets S12, S21 to zero
+func CombineTwoReflectiveStandardsPocket(r1, r2 []pocket.SParam) []pocket.SParam {
+
+	p := []pocket.SParam{}
+
+	for i := range r1 {
+		p = append(p, pocket.SParam{
+			Freq: r1[i].Freq,
+			S11:  r1[i].S11,
+			S22:  r2[i].S22,
+		})
+	}
+
+	return p
+
+}
+
+// PocketToCalibration changes the array structure to better suit our
+// usages of the results (e.g. calibration calculations, and presentation
+// in the user interface both need a frequency list so let's prepare that now
+// for efficiency)
+func PocketToCalibration(p []pocket.SParam) ([]uint64, SParam) {
+
+	// we'll use append rather than assuming max-length array
+	var freq []uint64
+	var s11_real, s11_imag, s12_real, s12_imag, s21_real, s21_imag, s22_real, s22_imag []float64
+
+	for _, param := range p {
+		freq = append(freq, param.Freq)
+		s11_real = append(s11_real, param.S11.Real)
+		s11_imag = append(s11_imag, param.S11.Imag)
+		s12_real = append(s12_real, param.S12.Real)
+		s12_imag = append(s12_imag, param.S12.Imag)
+		s21_real = append(s21_real, param.S21.Real)
+		s21_imag = append(s21_imag, param.S21.Imag)
+		s22_real = append(s22_real, param.S22.Real)
+		s22_imag = append(s22_imag, param.S22.Imag)
+	}
+
+	ca := SParam{
+		S11: ComplexArray{
+			Real: s11_real,
+			Imag: s11_imag,
+		},
+		S12: ComplexArray{
+			Real: s12_real,
+			Imag: s12_imag,
+		},
+		S21: ComplexArray{
+			Real: s21_real,
+			Imag: s21_imag,
+		},
+		S22: ComplexArray{
+			Real: s22_real,
+			Imag: s22_imag,
+		},
+	}
+	return freq, ca
+
+}
+
+func PocketToResult(p []pocket.SParam) Result {
+	freq, ca := PocketToCalibration(p)
+	return Result{
+		Freq: freq,
+		S11:  ca.S11,
+		S12:  ca.S12,
+		S21:  ca.S21,
+		S22:  ca.S22,
+	}
+}
+
+// CalibrationToPocket is used by the middleware
+func CalibrationToPocket(result Result) ([]pocket.SParam, error) {
+
+	pa := []pocket.SParam{}
+
+	if len(result.Freq) != len(result.S11.Real) { //assume real is same length as imag
+		return pa, errors.New("Freq and S11 are different lengths")
+	}
+	if len(result.Freq) != len(result.S12.Real) {
+		return pa, errors.New("Freq and S12 are different lengths")
+	}
+	if len(result.Freq) != len(result.S21.Real) {
+		return pa, errors.New("Freq and S21 are different lengths")
+	}
+	if len(result.Freq) != len(result.S22.Real) {
+		return pa, errors.New("Freq and S22 are different lengths")
+	}
+
+	for i, freq := range result.Freq {
+		p := pocket.SParam{
+			Freq: freq,
+			S11: pocket.Complex{
+				Real: result.S11.Real[i],
+				Imag: result.S11.Imag[i],
+			},
+			S12: pocket.Complex{
+				Real: result.S12.Real[i],
+				Imag: result.S12.Imag[i],
+			},
+			S21: pocket.Complex{
+				Real: result.S21.Real[i],
+				Imag: result.S21.Imag[i],
+			},
+			S22: pocket.Complex{
+				Real: result.S22.Real[i],
+				Imag: result.S22.Imag[i],
+			},
+		}
+		pa = append(pa, p)
+	}
+
+	return pa, nil
+}
+
 // New creates a Calibration object and the channels
 // needed to communicate with the calibration server
 // which are request, response
@@ -194,6 +470,13 @@ func (c *Calibration) Set(target string, result Result) error {
 		}
 	}
 
+	/*sp := SParam{
+		S11: result.S11,
+		S12: result.S12,
+		S21: result.S21,
+		S22: result.S22,
+	}*/
+
 	switch {
 	case target == "short":
 		c.Command.Short = result
@@ -229,25 +512,52 @@ func (ca *ComplexArray) BadLen(freq []uint64) bool {
 	return false
 }
 
+// no need for errors with messages - should have found these out by now -
+// this is mainly a check that all required elements are present
+// but adding the belt and braces of checking individual array lengths
+func (r *Result) LengthEquals(required int) bool {
+
+	rl := []int{
+		len(r.Freq),
+		len(r.S11.Real),
+		len(r.S11.Imag),
+		len(r.S12.Real),
+		len(r.S12.Imag),
+		len(r.S21.Real),
+		len(r.S21.Imag),
+		len(r.S22.Real),
+		len(r.S22.Imag),
+	}
+
+	for _, l := range rl {
+		if l != required {
+			return false
+		}
+	}
+
+	return true
+}
+
 func (c *Calibration) Apply() (Result, error) {
 
 	result := Result{}
+	required := len(c.Command.Freq)
 
-	if c.Command.Short.BadLen(c.Command.Freq) {
+	if !c.Command.Short.LengthEquals(required) {
 		return result, errors.New("Wrong number of samples in Short data")
 	}
 
-	if c.Command.Open.BadLen(c.Command.Freq) {
+	if !c.Command.Open.LengthEquals(required) {
 		return result, errors.New("Wrong number of samples in Open data")
 	}
 
-	if c.Command.Load.BadLen(c.Command.Freq) {
+	if !c.Command.Load.LengthEquals(required) {
 		return result, errors.New("Wrong number of samples in Load data")
 	}
-	if c.Command.Thru.BadLen(c.Command.Freq) {
+	if !c.Command.Thru.LengthEquals(required) {
 		return result, errors.New("Wrong number of samples in Thru data")
 	}
-	if c.Command.DUT.BadLen(c.Command.Freq) {
+	if !c.Command.DUT.LengthEquals(required) {
 		return result, errors.New("Wrong number of samples in DUT data")
 	}
 
--- pkg/calibration/calibration_test.go
+++ pkg/calibration/calibration_test.go
@@ -3,6 +3,7 @@ package calibration
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -15,6 +16,7 @@ import (
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
+	"github.com/timdrysdale/go-pocketvna/pkg/pocket"
 	"github.com/timdrysdale/go-pocketvna/pkg/reconws"
 )
 
@@ -25,6 +27,318 @@ func init() {
 }
 
 var upgrader = websocket.Upgrader{}
+
+type testDataPocket struct {
+	s1 []pocket.SParam
+	s2 []pocket.SParam
+	s  []pocket.SParam
+	o1 []pocket.SParam
+	o2 []pocket.SParam
+	o  []pocket.SParam
+	l1 []pocket.SParam
+	l2 []pocket.SParam
+	l  []pocket.SParam
+	t  []pocket.SParam
+	d  []pocket.SParam
+}
+
+type testDataResult struct {
+	s1 Result
+	s2 Result
+	o1 Result
+	o2 Result
+	l1 Result
+	l2 Result
+	t  Result
+	d  Result
+	z  Result
+}
+
+func makeData(t *testing.T) (testDataPocket, testDataResult) {
+
+	// Make some results in a convenient form (lists)
+	// make each value unique to check for
+	// typos in assignments of values in structs
+	freq := []uint64{1000, 2000, 3000}
+	_a := []float64{0.1, 0.2, 0.3}
+	_b := []float64{0.4, 0.5, 0.6}
+	_c := []float64{0.7, 0.8, 0.9}
+	_d := []float64{1.0, 1.1, 1.2}
+	_e := []float64{1.3, 1.4, 1.5}
+	_f := []float64{1.6, 1.7, 1.8}
+	_g := []float64{1.9, 2.0, 2.1}
+	_h := []float64{2.2, 2.3, 2.4}
+	_i := []float64{2.5, 2.6, 2.7}
+	_j := []float64{2.8, 2.9, 3.0}
+	_k := []float64{3.1, 3.2, 3.3}
+	_l := []float64{3.4, 3.5, 3.6}
+	_m := []float64{3.8, 3.9, 4.0}
+	_n := []float64{4.1, 4.2, 4.3}
+	_o := []float64{4.4, 4.5, 4.6}
+	_p := []float64{4.7, 4.8, 4.9}
+	_q := []float64{5.0, 5.1, 5.2}
+	_r := []float64{5.3, 5.4, 5.5}
+	_s := []float64{5.6, 5.7, 5.8}
+	_t := []float64{5.9, 6.0, 6.1}
+	_u := []float64{6.2, 6.3, 6.4}
+	_v := []float64{6.5, 6.6, 6.7}
+	_w := []float64{6.8, 6.9, 7.0}
+	_x := []float64{7.1, 7.2, 7.3}
+	_y := []float64{7.4, 7.5, 7.6}
+	_z := []float64{7.7, 7.8, 7.9}
+	_aa := []float64{8.0, 8.1, 8.2}
+	_ab := []float64{8.3, 8.4, 8.5}
+
+	z := make([]float64, 3)
+	caz := ComplexArray{
+		Imag: z,
+		Real: z,
+	}
+
+	rz := Result{
+		Freq: freq,
+		S11:  caz,
+		S12:  caz,
+		S21:  caz,
+		S22:  caz,
+	}
+
+	rs1 := rz
+	rs1.S11.Imag = _a
+	rs1.S11.Real = _b
+
+	rs2 := rz
+	rs2.S22.Imag = _c
+	rs2.S22.Real = _d
+
+	ro1 := rz
+	ro1.S11.Imag = _e
+	ro1.S11.Real = _f
+
+	ro2 := rz
+	ro2.S22.Imag = _g
+	ro2.S22.Real = _h
+
+	rl1 := rz
+	rl1.S11.Imag = _i
+	rl1.S11.Real = _j
+
+	rl2 := rz
+	rl2.S22.Imag = _k
+	rl2.S22.Real = _l
+
+	rt := Result{
+		Freq: freq,
+		S11: ComplexArray{
+			Imag: _m,
+			Real: _n,
+		},
+		S12: ComplexArray{
+			Imag: _o,
+			Real: _p,
+		},
+		S21: ComplexArray{
+			Imag: _q,
+			Real: _r,
+		},
+		S22: ComplexArray{
+			Imag: _s,
+			Real: _t,
+		},
+	}
+
+	rd := Result{
+		Freq: freq,
+		S11: ComplexArray{
+			Imag: _u,
+			Real: _v,
+		},
+		S12: ComplexArray{
+			Imag: _w,
+			Real: _x,
+		},
+		S21: ComplexArray{
+			Imag: _y,
+			Real: _z,
+		},
+		S22: ComplexArray{
+			Imag: _aa,
+			Real: _ab,
+		},
+	}
+
+	// convert into pocket form
+	pshort1, err := makePocketSParam(rs1)
+	assert.NoError(t, err)
+	pshort2, err := makePocketSParam(rs2)
+	assert.NoError(t, err)
+	popen1, err := makePocketSParam(ro1)
+	assert.NoError(t, err)
+	popen2, err := makePocketSParam(ro2)
+	assert.NoError(t, err)
+	pload1, err := makePocketSParam(rl1)
+	assert.NoError(t, err)
+	pload2, err := makePocketSParam(rl2)
+	assert.NoError(t, err)
+	pthru, err := makePocketSParam(rt)
+	assert.NoError(t, err)
+	pdut, err := makePocketSParam(rd)
+	assert.NoError(t, err)
+	// Test cal functions
+
+	pshort := CombineTwoReflectiveStandardsPocket(pshort1, pshort2)
+	popen := CombineTwoReflectiveStandardsPocket(popen1, popen2)
+	pload := CombineTwoReflectiveStandardsPocket(pload1, pload2)
+
+	tdp := testDataPocket{
+		s1: pshort1,
+		s2: pshort2,
+		s:  pshort,
+		o1: popen1,
+		o2: popen2,
+		o:  popen,
+		l1: pload1,
+		l2: pload2,
+		l:  pload,
+		t:  pthru,
+		d:  pdut,
+	}
+
+	tdr := testDataResult{
+		s1: rs1,
+		s2: rs2,
+		o1: ro1,
+		o2: ro2,
+		l1: rl1,
+		l2: rl2,
+		t:  rt,
+		d:  rd,
+		z:  rz,
+	}
+
+	return tdp, tdr
+}
+
+func makeCommand(t *testing.T) (Command, error) {
+
+	tdp, _ := makeData(t)
+
+	return MakeTwoPort(tdp.s1, tdp.s2, tdp.o1, tdp.o2, tdp.l1, tdp.l2, tdp.t, tdp.d)
+
+}
+
+func makeSParamS11(freq []uint64, real, imag []float64) ([]pocket.SParam, error) {
+
+	if len(freq) != len(real) || len(freq) != len(imag) {
+		return []pocket.SParam{}, errors.New("Freq/real/imag inconsistent length")
+	}
+
+	pa := []pocket.SParam{}
+
+	for i := range freq {
+		p := pocket.SParam{
+			Freq: freq[i],
+			S11: pocket.Complex{
+				Imag: imag[i],
+				Real: real[i],
+			},
+		}
+		pa = append(pa, p)
+	}
+
+	return pa, nil
+}
+func makePocketSParam(r Result) ([]pocket.SParam, error) {
+
+	pa := []pocket.SParam{}
+
+	for i := range r.Freq {
+		p := pocket.SParam{
+			Freq: r.Freq[i],
+			S11: pocket.Complex{
+				Imag: r.S11.Imag[i],
+				Real: r.S11.Real[i],
+			},
+			S12: pocket.Complex{
+				Imag: r.S12.Imag[i],
+				Real: r.S12.Real[i],
+			},
+			S21: pocket.Complex{
+				Imag: r.S21.Imag[i],
+				Real: r.S21.Real[i],
+			},
+			S22: pocket.Complex{
+				Imag: r.S22.Imag[i],
+				Real: r.S22.Real[i],
+			},
+		}
+		pa = append(pa, p)
+	}
+
+	return pa, nil
+}
+
+func TestMakeData(t *testing.T) {
+
+	freq := []uint64{1000, 2000, 3000}
+	_a := []float64{0.1, 0.2, 0.3}
+	_b := []float64{0.4, 0.5, 0.6}
+	/*
+		_c := []float64{0.7, 0.8, 0.9}
+		_d := []float64{1.0, 1.1, 1.2}
+		_e := []float64{1.3, 1.4, 1.5}
+		_f := []float64{1.6, 1.7, 1.8}
+		_g := []float64{1.9, 2.0, 2.1}
+		_h := []float64{2.2, 2.3, 2.4}
+		_i := []float64{2.5, 2.6, 2.7}
+		_j := []float64{2.8, 2.9, 3.0}
+		_k := []float64{3.1, 3.2, 3.3}
+		_l := []float64{3.4, 3.5, 3.6}
+		_m := []float64{3.8, 3.9, 4.0}
+		_n := []float64{4.1, 4.2, 4.3}
+		_o := []float64{4.4, 4.5, 4.6}
+		_p := []float64{4.7, 4.8, 4.9}
+		_q := []float64{5.0, 5.1, 5.2}
+		_r := []float64{5.3, 5.4, 5.5}
+		_s := []float64{5.6, 5.7, 5.8}
+		_t := []float64{5.9, 6.0, 6.1}
+		_u := []float64{6.2, 6.3, 6.4}
+		_v := []float64{6.5, 6.6, 6.7}
+		_w := []float64{6.8, 6.9, 7.0}
+		_x := []float64{7.1, 7.2, 7.3}
+		_y := []float64{7.4, 7.5, 7.6}
+		_z := []float64{7.7, 7.8, 7.9}
+		_aa := []float64{8.0, 8.1, 8.2}
+		_ab := []float64{8.3, 8.4, 8.5}
+	*/
+	tdp, _ := makeData(t)
+
+	// Check frequencies are correct in sparam results
+	assert.Equal(t, freq[0], tdp.s[0].Freq)
+	assert.Equal(t, freq[1], tdp.s[1].Freq)
+	assert.Equal(t, freq[2], tdp.s[2].Freq)
+	assert.Equal(t, freq[0], tdp.o[0].Freq)
+	assert.Equal(t, freq[1], tdp.o[1].Freq)
+	assert.Equal(t, freq[2], tdp.o[2].Freq)
+	assert.Equal(t, freq[0], tdp.l[0].Freq)
+	assert.Equal(t, freq[1], tdp.l[1].Freq)
+	assert.Equal(t, freq[2], tdp.l[2].Freq)
+	assert.Equal(t, freq[0], tdp.t[0].Freq)
+	assert.Equal(t, freq[1], tdp.t[1].Freq)
+	assert.Equal(t, freq[2], tdp.t[2].Freq)
+	assert.Equal(t, freq[0], tdp.d[0].Freq)
+	assert.Equal(t, freq[1], tdp.d[1].Freq)
+	assert.Equal(t, freq[2], tdp.d[2].Freq)
+
+	// Check selected results
+	assert.Equal(t, _a[0], tdp.s[0].S11.Imag)
+	assert.Equal(t, _a[1], tdp.s[1].S11.Imag)
+	assert.Equal(t, _a[2], tdp.s[2].S11.Imag)
+	assert.Equal(t, _b[0], tdp.s[0].S11.Real)
+	assert.Equal(t, _b[1], tdp.s[1].S11.Real)
+	assert.Equal(t, _b[2], tdp.s[2].S11.Real)
+
+}
 
 func TestNew(t *testing.T) {
 
@@ -57,50 +371,44 @@ func TestNew(t *testing.T) {
 	default:
 	}
 
-	freq := []uint64{1000, 2000, 3000}
-	short_real := []float64{0.31, 0.32, -0.33}
-	short_imag := []float64{0.41, -0.42, 0.45}
-	open_real := []float64{1.31, 1.32, -1.33}
-	open_imag := []float64{1.41, -1.42, 1.45}
-	load_real := []float64{2.31, 2.32, -2.33}
-	load_imag := []float64{2.41, -2.42, 2.45}
-	dut_real := []float64{3.31, 3.32, -3.33}
-	dut_imag := []float64{3.41, -3.42, 3.45}
+	// Get some data
 
-	pshort, err := makeSParam(freq, short_real, short_imag)
+	tdp, tdr := makeData(t)
+
+	err := cal.SetShortParam(tdp.s)
 	assert.NoError(t, err)
 
-	popen, err := makeSParam(freq, open_real, open_imag)
+	err = cal.SetLoadParam(tdp.l)
 	assert.NoError(t, err)
 
-	pload, err := makeSParam(freq, load_real, load_imag)
+	err = cal.SetOpenParam(tdp.o)
 	assert.NoError(t, err)
 
-	pdut, err := makeSParam(freq, dut_real, dut_imag)
+	err = cal.SetThruParam(tdp.t)
 	assert.NoError(t, err)
 
-	err = cal.SetShortParam(pshort)
-	assert.NoError(t, err)
-
-	err = cal.SetLoadParam(pload)
-	assert.NoError(t, err)
-
-	err = cal.SetOpenParam(popen)
-	assert.NoError(t, err)
-
-	err = cal.SetDUTParam(pdut)
+	err = cal.SetDUTParam(tdp.d)
 	assert.NoError(t, err)
 
 	// check data is correctly assigned
-	assert.Equal(t, freq, cal.Command.Freq)
-	assert.Equal(t, short_real, cal.Command.Short.Real)
-	assert.Equal(t, short_imag, cal.Command.Short.Imag)
-	assert.Equal(t, open_real, cal.Command.Open.Real)
-	assert.Equal(t, open_imag, cal.Command.Open.Imag)
-	assert.Equal(t, load_real, cal.Command.Load.Real)
-	assert.Equal(t, load_imag, cal.Command.Load.Imag)
-	assert.Equal(t, dut_real, cal.Command.DUT.Real)
-	assert.Equal(t, dut_imag, cal.Command.DUT.Imag)
+	assert.Equal(t, tdr.s1.Freq, cal.Command.Freq)
+	assert.Equal(t, tdr.s1.S11, cal.Command.Short.S11)
+	assert.Equal(t, tdr.z.S12, cal.Command.Short.S12)
+	assert.Equal(t, tdr.z.S21, cal.Command.Short.S21)
+	assert.Equal(t, tdr.s2.S22, cal.Command.Short.S22)
+
+	assert.Equal(t, tdr.o1.S11, cal.Command.Open.S11)
+	assert.Equal(t, tdr.z.S12, cal.Command.Open.S12)
+	assert.Equal(t, tdr.z.S21, cal.Command.Open.S21)
+	assert.Equal(t, tdr.o2.S22, cal.Command.Open.S22)
+
+	assert.Equal(t, tdr.l1.S11, cal.Command.Load.S11)
+	assert.Equal(t, tdr.z.S12, cal.Command.Load.S12)
+	assert.Equal(t, tdr.z.S21, cal.Command.Load.S21)
+	assert.Equal(t, tdr.l2.S22, cal.Command.Load.S22)
+
+	assert.Equal(t, tdr.t, cal.Command.Thru)
+	assert.Equal(t, tdr.d, cal.Command.DUT)
 
 	// send data to service
 
@@ -109,44 +417,7 @@ func TestNew(t *testing.T) {
 	assert.NoError(t, err)
 
 	// the mock returns the original DUT data as the supposedly calibrated data
-	assert.Equal(t, dut_real, result.S11.Real)
-	assert.Equal(t, dut_imag, result.S11.Imag)
-
-}
-
-func makeCommand() (Command, error) {
-
-	freq := []uint64{1000, 2000, 3000}
-	short_real := []float64{0.31, 0.32, -0.33}
-	short_imag := []float64{0.41, -0.42, 0.45}
-	open_real := []float64{1.31, 1.32, -1.33}
-	open_imag := []float64{1.41, -1.42, 1.45}
-	load_real := []float64{2.31, 2.32, -2.33}
-	load_imag := []float64{2.41, -2.42, 2.45}
-	dut_real := []float64{3.31, 3.32, -3.33}
-	dut_imag := []float64{3.41, -3.42, 3.45}
-
-	pshort, err := makeSParam(freq, short_real, short_imag)
-	if err != nil {
-		return Command{}, err
-	}
-
-	popen, err := makeSParam(freq, open_real, open_imag)
-	if err != nil {
-		return Command{}, err
-	}
-
-	pload, err := makeSParam(freq, load_real, load_imag)
-	if err != nil {
-		return Command{}, err
-	}
-
-	pdut, err := makeSParam(freq, dut_real, dut_imag)
-	if err != nil {
-		return Command{}, err
-	}
-
-	return MakeOnePort(pshort, popen, pload, pdut)
+	assert.Equal(t, tdr.d, result)
 
 }
 
@@ -161,7 +432,7 @@ func TestPipeInterfaceToWs(t *testing.T) {
 
 	/* Test Request */
 
-	c, err := makeCommand()
+	c, err := makeCommand(t)
 
 	assert.NoError(t, err)
 
@@ -233,6 +504,82 @@ func TestPipeWsToInterface(t *testing.T) {
 
 }
 
+func TestConvertPocketToCalibration(t *testing.T) {
+
+	tdp, tdr := makeData(t)
+
+	freq, sparam := PocketToCalibration(tdp.t)
+
+	assert.Equal(t, tdr.t.Freq, freq)
+	assert.Equal(t, tdr.t.S11, sparam.S11)
+	assert.Equal(t, tdr.t.S12, sparam.S12)
+	assert.Equal(t, tdr.t.S21, sparam.S21)
+	assert.Equal(t, tdr.t.S22, sparam.S22)
+}
+
+func TestMakeTwoPort(t *testing.T) {
+
+	tdp, tdr := makeData(t)
+
+	// Check works with good data
+	tp, err := MakeTwoPort(tdp.s1, tdp.s2, tdp.o1, tdp.o2, tdp.l1, tdp.l2, tdp.t, tdp.d)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, tdr.s1.Freq, tp.Freq)
+	assert.Equal(t, tdr.s1.S11, tp.Short.S11)
+	assert.Equal(t, tdr.z.S12, tp.Short.S12)
+	assert.Equal(t, tdr.z.S21, tp.Short.S21)
+	assert.Equal(t, tdr.s2.S22, tp.Short.S22)
+
+	assert.Equal(t, tdr.o1.S11, tp.Open.S11)
+	assert.Equal(t, tdr.z.S12, tp.Open.S12)
+	assert.Equal(t, tdr.z.S21, tp.Open.S21)
+	assert.Equal(t, tdr.o2.S22, tp.Open.S22)
+
+	assert.Equal(t, tdr.l1.S11, tp.Load.S11)
+	assert.Equal(t, tdr.z.S12, tp.Load.S12)
+	assert.Equal(t, tdr.z.S21, tp.Load.S21)
+	assert.Equal(t, tdr.l2.S22, tp.Load.S22)
+
+	assert.Equal(t, tdr.t, tp.Thru)
+	assert.Equal(t, tdr.d, tp.DUT)
+
+	// Check throws error if data lengths are different
+	_, err = MakeTwoPort([]pocket.SParam{}, tdp.s2, tdp.o1, tdp.o2, tdp.l1, tdp.l2, tdp.t, tdp.d)
+	assert.Error(t, err)
+
+}
+
+func TestCalibrationToPocket(t *testing.T) {
+
+	tdp, tdr := makeData(t)
+
+	pt, err := CalibrationToPocket(tdr.t)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, tdp.t, pt)
+	/*
+		for i := range tdr.t.Freq {
+			assert.Equal(t, tdr.t.Freq[i], pt[i].Freq)
+			assert.Equal(t, tdr.t.S11[i], pt[i].S11)
+			assert.Equal(t, tdr.t.S12[i], pt[i].S12)
+			assert.Equal(t, tdr.t.S21[i], pt[i].S21)
+			assert.Equal(t, tdr.t.S22[i], pt[i].S22)
+		}*/
+
+	badt := tdr.t
+
+	badt.S12 = ComplexArray{}
+
+	_, err = CalibrationToPocket(badt)
+
+	assert.Error(t, err)
+	assert.Equal(t, "Freq and S12 are different lengths", err.Error())
+
+}
+
 //
 //// Reply with expected response if port command is correctly formed
 //
@@ -265,7 +612,10 @@ func calMock(request, response chan reconws.WsMessage, ctx context.Context) {
 
 				r := Result{
 					Freq: c.Freq,
-					S11:  c.DUT,
+					S11:  c.DUT.S11,
+					S12:  c.DUT.S12,
+					S21:  c.DUT.S21,
+					S22:  c.DUT.S22,
 				}
 
 				msg, err := json.Marshal(r)
