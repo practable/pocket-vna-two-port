74833169fb898771430587fe1f88c9cf557b168a
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-12-01 22:32:21 +0000

    build.sh (fix): buildtime

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export GOOS=linux
 now=$(date +'%Y-%m-%d_%T') #no spaces to prevent quotation issues in build command
-(cd ./cmd/vna; go build -ldflags "-X 'github.com/practable/pocket-vna-two-port/cmd/vna/cmd.Version=`git describe --tags`' -X 'github.com/practable/pocket-vna-two-port/cmd/relay/cmd.BuildTime=$now'"; ./vna version)
+(cd ./cmd/vna; go build -ldflags "-X 'github.com/practable/pocket-vna-two-port/cmd/vna/cmd.Version=`git describe --tags`' -X 'github.com/practable/pocket-vna-two-port/cmd/vna/cmd.BuildTime=$now'"; ./vna version)
