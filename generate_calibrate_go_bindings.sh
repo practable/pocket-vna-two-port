e2b0833acb1a93c63dcbe4e98a1860f5d83910a7
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-12-15 14:20:35 +0000

    py (fix): upgrade to py3.12 & grpcio 1.60

--- generate_calibrate_go_bindings.sh
+++ generate_calibrate_go_bindings.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
- protoc --go_out=./pkg/pb --go_opt=paths=source_relative \
-    --go-grpc_out=./pkg/pb --go-grpc_opt=paths=source_relative \
-    calibrate.proto
+#needs recent version of grpcio to match container image, e.g. conda activate py3.12
+protoc --go_out=./pkg/pb --go_opt=paths=source_relative \
+   --go-grpc_out=./pkg/pb --go-grpc_opt=paths=source_relative \
+   calibrate.proto
