b0af1fd08421f6011ff5fe812a2735494674c52c
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-12-18 22:24:42 +0000

    py (fix): docker tag in app-linux-arm32/push.sh

--- py/docker/app-linux-arm32/push.sh
+++ py/docker/app-linux-arm32/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker tag calibration-twoport-grpc:latest practable/calibration-twoport-grpc:arm64v8-3.12-0.2
-docker push practable/calibration-twoport-grpc:arm64v8-3.12-0.2
+docker tag calibration-twoport-grpc:latest practable/calibration-twoport-grpc:arm32v7-3.12-0.2
+docker push practable/calibration-twoport-grpc:arm32v7-3.12-0.2
