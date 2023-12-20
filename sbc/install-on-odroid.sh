0a5bcefe7ad3ad96ddbb7234ac1ab8ea5bd2bde4
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-12-20 14:02:29 +0000

    sbc (fix): relay tokens via install-on-odroid

--- sbc/install-on-odroid.sh
+++ sbc/install-on-odroid.sh
@@ -2,6 +2,7 @@
 
 # we assume jump is already installed
 # we assume that relay access and token files are already present in /etc/practable
+# assume that /etc/practable/id has the correct id for the experiment, e.g. pvna05
 
 cp ./files/vna-data /usr/local/bin/
 cp ./files/relay-rules /usr/local/bin/
@@ -17,6 +18,13 @@ arduino-cli lib install timerinterrupt
 cd ./fw
 arduino-cli compile --fqbn arduino:avr:nano RFSwitch/ 
 arduino-cli upload --port /dev/ttyUSB0 --fqbn arduino:avr:nano RFSwitch/
+
+#get relay tokens
+export FILES=$(/home/odroid/files.link)
+export PRACTABLE_ID=$(cat /etc/practable/id)
+cd /etc/practable
+wget $FILES/st-ed0-data.access.$PRACTABLE_ID -O  st-ed0-data.access
+wget $FILES/st-ed0-data.token.$PRACTABLE_ID -O   st-ed0-data.token
 
 # start services
 systemctl enable calibration.service
